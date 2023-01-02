cf7b6f47925e47c367c82c0479e7120c6afac6f3
Paul Mackay <pauljmackay@gmail.com>
2023-01-02 09:32:54 +0000

    Add entity dates

--- plugins/builders/airtable_data.rb
+++ plugins/builders/airtable_data.rb
@@ -1,8 +1,14 @@
 class Builders::AirtableData < SiteBuilder
   def build
     puts config["airtable_api_key"]
-    url = "https://api.airtable.com/v0/app5RG1gIK9buK1iC/events?api_key=#{config["airtable_api_key"]}"
-    # get , headers: {"Authorization" => "Bearer #{config["airtable_api_key"]}"} do |data|
+    build_events
+    build_entities
+  end
+
+  private
+
+  def build_events
+    url = base_url("events")
     get url do |data|
       data["records"].each do |event|
         # puts event.to_s
@@ -17,5 +23,27 @@ class Builders::AirtableData < SiteBuilder
         end
       end
     end
+  end
+
+  def build_entities
+    url = base_url("entities")
+    get url do |data|
+      data["records"].each do |event|
+        # puts event.to_s
+        fields = event["fields"]
+        fields["start"] = fields["startDate"]
+        fields["content"] = "#{fields["name"]} began"
+        fields["group"] = 1 # local
+        add_resource :events, "#{event["id"]}" do
+          fields fields
+          title fields["title"]
+          id event["id"]
+        end
+      end
+    end
+  end
+
+  def base_url(base_name)
+    "https://api.airtable.com/v0/app5RG1gIK9buK1iC/#{base_name}?api_key=#{config["airtable_api_key"]}"
   end
 end
